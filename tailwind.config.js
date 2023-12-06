8aa86b9a1cbb41be9316035d461fd02a82fd90b4
Dimuth Nilanjana <106858400+Dimuthnilanjana@users.noreply.github.com>
2023-12-06 18:47:57 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ['class', '[data-mode="dark"]'],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        poppins:["poppins", "sans-serif"],
+
+      }, 
+    },
+  },
+  plugins: [],
+};
